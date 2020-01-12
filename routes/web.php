c558cca341ed27ce4a1584400d07f7c7e54566a9
KingAbesh <abeshekwere@gmail.com>
2020-01-12 04:05:48 +0100

    templating

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,8 @@
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+ */
 
-Route::get('/posts/{post}', 'PostsController@show');
+Route::get('', function () {
+    return view('welcome');
+});
